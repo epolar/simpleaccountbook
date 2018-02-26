4130047fc335ed5c7a9024274bfaed6feff4fef1
eraise <eraise@qq.com>
2018-02-26 15:03:29 +0800

    添加 Android-Debug-Database 库方便查看数据

--- app/src/main/java/xyz/eraise/simpleaccountbook/repository/databases/SQLCipherHelperImpl.kt
+++ app/src/main/java/xyz/eraise/simpleaccountbook/repository/databases/SQLCipherHelperImpl.kt
@@ -3,6 +3,8 @@ package xyz.eraise.simpleaccountbook.repository.databases
 import com.raizlabs.android.dbflow.config.DatabaseDefinition
 import com.raizlabs.android.dbflow.sqlcipher.SQLCipherOpenHelper
 import com.raizlabs.android.dbflow.structure.database.DatabaseHelperListener
+import xyz.eraise.simpleaccountbook.MyApp
+import xyz.eraise.simpleaccountbook.R
 
 /**
  * Created by eraise on 2018/2/9.
@@ -11,6 +13,7 @@ class SQLCipherHelperImpl(databaseDefinition: DatabaseDefinition,
                           databaseHelperListener: DatabaseHelperListener?)
     : SQLCipherOpenHelper(databaseDefinition, databaseHelperListener) {
 
-    override fun getCipherSecret(): String = "SimpleAccountBook"
+    override fun getCipherSecret(): String =
+            MyApp.instance.getString(R.string.DB_PASSWORD_APPDATABASE)
 
 }
