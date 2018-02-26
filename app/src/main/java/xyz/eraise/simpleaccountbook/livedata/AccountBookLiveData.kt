1df59f0be96e07280ac7d91808bfda8fa2febd9e
eraise <eraise@qq.com>
2018-02-26 13:46:11 +0800

    去除不需要的包

--- app/src/main/java/xyz/eraise/simpleaccountbook/livedata/AccountBookLiveData.kt
+++
@@ -1,9 +0,0 @@
-package xyz.eraise.simpleaccountbook.livedata
-
-import android.arch.lifecycle.LiveData
-import xyz.eraise.simpleaccountbook.pojo.AccountBook
-
-/**
- * Created by eraise on 2018/1/23.
- */
-class AccountBookLiveData: LiveData<AccountBook>()
